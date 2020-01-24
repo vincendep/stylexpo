e53b09d350826c04b9abb9e0d06845430bfcf679
vincendep <vincenzo.depetris@student.univaq.it>
2020-01-24 15:01:56 +0100

    Add 404 custom page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,33 @@
+@extends('master')
+
+@section('title', '404')
+
+@section('open body')
+<div class="se-pre-con"></div>
+<div class="main">
+@endsection
+
+  @section('content')
+  <section class="ptb-70 gray-bg error-block-main">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="error-block-detail error-block-bg">
+            <div class="row">
+              <div class="col-xl-5 col-lg-6"></div>
+              <div class="col-xl-7 col-lg-6">
+                <div class="main-error-text">404</div>
+                <div class="error-small-text">We are Sorry</div>
+                <div class="error-slogan">The page you Are Looking for does not Exist</div>
+                <div class="middle-580">
+                  <p>Oh, would I could describe these conceptions, could impress upon paper all that is living so full and warm within me, that it might be the mirror of my soul, as my soul is the mirror of the infinite God!</p>
+                </div>
+                <div class="mt-40"> <a href="/" class="btn btn-color">Back to Home</a> </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  @endsection
